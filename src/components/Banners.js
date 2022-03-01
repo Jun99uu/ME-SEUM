40b7480d463b5be741d802bcd61ff03a60d5c252
Jun99uu <igun0423@naver.com>
2022-03-01 23:20:00 +0900

    Contents / Banner 구상

---
+++ src/components/Banners.js
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+
+function Banners({ setOpened, image, date, link, content }) {
+  return <div></div>;
+}
+
+export default Banners;
---
+++ src/components/Contents.js
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+
+function Contents({ setOpened, image, date, content }) {
+  return <div></div>;
+}
+
+export default Contents;
